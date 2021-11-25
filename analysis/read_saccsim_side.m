5cb9d6a8396f00720c8ca52a5a2de4be5b688fd0
Seb James <seb.james@sheffield.ac.uk>
2021-11-25 12:06:35 +0000

    A better example for reading saccade movement data

---
+++ analysis/read_saccsim_side.m
@@ -0,0 +1,30 @@
+% Here's example code to show when the pro- or anti-saccade occurred
+
+stim_on = 350;
+min_outlier = 180;
+model_log_path = '/home/seb/SpineML_2_BRAHMS/temp/Oculomotor_Model_lumcomp_arith_e5';
+SS = csvread ( [model_log_path '/run/saccsim_side.log'], 1 , 0);
+eyeRx = SS(:,8);
+eyeRy = SS(:,9);
+eyeRz = SS(:,10);
+eyeTime = SS(:,1);
+clear SS;
+
+PS_RT_absolute = min( find( eyeRy > 0 ) );
+AS_RT_absolute = min( find( eyeRy < 0 ) );
+if ~isempty( PS_RT_absolute )
+    PS_RT = PS_RT_absolute - stim_on;
+    printf ('Prosaccade response after %d ms\n', PS_RT)
+    if PS_RT < min_outlier
+        % Anomaly
+        display ('That was a fast response...')
+    end
+end
+if ~isempty( AS_RT_absolute )
+    AS_RT = AS_RT_absolute - stim_on;
+    printf ('Antisaccade response after %d ms\n', AS_RT)
+    if AS_RT < min_outlier
+        % Anomaly
+        display ('That was a fast response...')
+    end
+end
